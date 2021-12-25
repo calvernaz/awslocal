630af330a844f778283665afc4679411195d2ef8
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2021-12-25 20:03:59 +0000

    profile fallback

--- functions/awslocal.fish
+++ functions/awslocal.fish
@@ -1,4 +1,5 @@
 function awslocal
 	  set -q LOCALHOST_HOST; or set LOCALHOST_HOST "http://localhost:4566"
-    aws --endpoint-url=$LOCALHOST_HOST
+		set -q AWS_PROFILE; or set AWS_PROFILE "localstack"
+    aws --profile $AWS_PROFILE --endpoint-url=$LOCALHOST_HOST $argv
 end
